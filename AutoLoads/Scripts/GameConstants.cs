d64decea25b3da732404001c2cec5392fe0a53b7
aniran <aniran530@gmail.com>
2025-03-13 22:48:31 +0530

    Implement player movement and input handling with jump functionality

--- AutoLoads/Scripts/GameConstants.cs
+++ AutoLoads/Scripts/GameConstants.cs
@@ -2,4 +2,10 @@
 
 public class GameConstants 
 {
+    public const string INPUT_LEFT = "move_left";
+    public const string INPUT_RIGHT = "move_right";
+    public const string INPUT_FORWARD = "move_forward";
+    public const string INPUT_BACK = "move_back";
+    public const string JUMP = "jump";
+    public const string INPUT_DASH = "dash";
 }
---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	// variables
+	[ Export ]
+	private float speed = 5.0f;
+	[ Export ]
+	private float jumpVelocity = 4.5f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed( GameConstants.JUMP ) && IsOnFloor())
+		{
+			velocity.Y = jumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector( GameConstants.INPUT_LEFT, GameConstants.INPUT_RIGHT, GameConstants.INPUT_FORWARD, GameConstants.INPUT_BACK );
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * speed;
+			velocity.Z = direction.Z * speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
